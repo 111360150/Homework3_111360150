1ad822912f07456e71993302455918e793724658
111360150 <t111360150@ntut.org.tw>
2024-11-14 19:36:13 +0800

    your commit content.

--- src/ch3/P07.java
+++ src/ch3/P07.java
@@ -12,10 +12,10 @@ public class P07 {
 		car2.num=2345;
 		car2.gas=30.5;
 		
-		System.out.println("車號是"+car1.num);
-		System.out.println("汽油量是"+car1.gas);
-		System.out.println("車號是"+car2.num);
-		System.out.println("汽油量是"+car2.gas);
+		System.out.println("car1車號是"+car1.num);
+		System.out.println("car1汽油量是"+car1.gas);
+		System.out.println("car2車號是"+car2.num);
+		System.out.println("car2汽油量是"+car2.gas);
 	}
 }
 class Car7{
